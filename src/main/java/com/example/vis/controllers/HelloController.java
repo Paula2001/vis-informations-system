a8443b28f86a9a1ab332eefc39cdb78b3adb7a8c
Paula <paula@pop-os.localdomain>
2022-12-02 12:41:36 +0100

    added VSB connection

--- src/main/java/com/example/vis/controllers/HelloController.java
+++ src/main/java/com/example/vis/controllers/HelloController.java
@@ -16,18 +16,18 @@ import java.sql.SQLException;
 public class HelloController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;user=MyUserName;password=*****;encrypt=false;";
-        try {
-            Connection con = DriverManager.getConnection(connectionUrl);
-            System.out.println(con);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//        String connectionUrl = "jdbc:sqlserver://dbsys.cs.vsb.cz;databaseName=dbsql;user=GOR0096;password=N2ifO78so89893My;encrypt=false;";
+//        try {
+//            Connection con = DriverManager.getConnection(connectionUrl);
+//            System.out.println(con);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
         PrintWriter out = response.getWriter();
         out.println("this is a test hellllo");
     }
