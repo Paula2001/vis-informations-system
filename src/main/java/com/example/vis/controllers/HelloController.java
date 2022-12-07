809c0b1455850c13174d6932067f161e3cd1fdab
Paula <paula@pop-os.localdomain>
2022-12-07 11:04:40 +0100

    fixed server issue

--- src/main/java/com/example/vis/controllers/HelloController.java
+++ src/main/java/com/example/vis/controllers/HelloController.java
@@ -1,15 +1,22 @@
 package com.example.vis.controllers;
 
 
-import java.sql.ResultSet;
 import java.io.PrintWriter;
 import java.io.IOException;
 import com.example.vis.models.AdminModel;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloController extends Controller{
+@WebServlet("/hello")
+public class HelloController extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
